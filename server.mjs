36fa6910a838ea998dc27ad8b35d47090970ef6d
Felipe Soares <felipediogenes87@gmail.com>
2024-07-26 14:39:44 -0300

    ..

--- server.mjs
+++ server.mjs
@@ -5,7 +5,6 @@ import varasDisponiveis from './varasFederais.js';
 import { executarConsulta } from './consulta.mjs'; // Importa a função de consulta
 import { getUltimaConsulta, getProximaConsulta, setUltimaConsulta, setProximaConsulta } from './dadosConsulta.mjs';
 
-
 // Obtém o caminho do diretório do arquivo atual
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
