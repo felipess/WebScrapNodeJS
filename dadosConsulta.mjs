cc6a124922f7808039b35058ee1a862aec90a332
Felipe Soares <felipediogenes87@gmail.com>
2024-07-26 17:22:50 -0300

    Labels Última consulta: e - Próxima consulta: ok

--- dadosConsulta.mjs
+++ dadosConsulta.mjs
@@ -2,17 +2,29 @@ let ultimaConsulta = ''; // Valor padrão inicial
 let proximaConsulta = '-'; // Valor padrão inicial
 
 export function getUltimaConsulta() {
+    console.log('Obtendo última consulta:', ultimaConsulta);
     return ultimaConsulta;
 }
 
 export function setUltimaConsulta(data) {
+    console.log('Definindo última consulta para:', data);
     ultimaConsulta = data;
+    // atualizarLabels(); // Atualiza os labels
 }
 
 export function getProximaConsulta() {
+    console.log('Obtendo próxima consulta:', proximaConsulta);
     return proximaConsulta;
 }
 
 export function setProximaConsulta(data) {
+    console.log('Definindo próxima consulta para:', data);
     proximaConsulta = data;
+    // atualizarLabels(); // Atualiza os labels
 }
+
+// // Função para atualizar os labels no DOM
+// function atualizarLabels() {
+//     document.getElementById('ultimaConsulta').textContent = `Última consulta: ${getUltimaConsulta()}`;
+//     document.getElementById('proximaConsulta').textContent = `Próxima consulta: ${getProximaConsulta()}`;
+// }
